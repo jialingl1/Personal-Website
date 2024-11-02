7584a81f04f177982208b63394e094dbe098ca6c
Jialing Liu <jialingl.liu@gmail.com>
2024-11-02 16:41:13 -0400

    added frontpage description

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -5,7 +5,10 @@ function FrontPage () {
 
     return (
         <div>
-            <h1>Jialing Liu's Website</h1>
+            <h1 style= {{fontSize: "50px", fontFamily: "fantasy"}}>Jialing Liu</h1>
+            <br></br>
+            <p style={{fontSize: "25px", fontFamily: "Gill Sans"}}>I'm a Chinese-Colombian who is passionate about language teaching, 
+                instructional design, curriculum development, and coding!</p>
         </div>
 
     );
